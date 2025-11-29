738c215727fd22b296c1d52d5847aff906f92511
GnonhouaN01 <ngolognonhoua@gmail.com>
2025-11-29 15:28:25 +0000

    feat:mise en du frontend

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends("layout")
+
+@section("content")
+    <h2 class="text-2xl font-bold mb-4">Dashboard</h2>
+
+    <a href="/books" class="bg-blue-600 text-white px-4 py-2 rounded">Voir les livres</a>
+@endsection
